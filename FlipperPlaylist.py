5fc7b98875f7375b5028d7aa2f14916c409b8adc
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 21:40:19 +0000

    add

--- FlipperPlaylist.py
+++ FlipperPlaylist.py
@@ -4,11 +4,12 @@ import os
 
 def test():
     # Prompt the user to enter a path for the folder
-    folder_path = input("Enter the path for the folder: ")
+    folder_path = str(input("Enter the path for the folder: "))
+    playlist_name = str(input("Enter a name for the playlist: "))
     # Create the text file in the current working directory
-    playlist_file = open("playlist.txt", "w")
+    playlist_file = open((playlist_name + ".txt"), "w")
     # Iterate through the files in the folder
-    playlist_file.write("# Playlist\n")
+    playlist_file.write("# " + playlist_name + "\n")
     for file in os.listdir(folder_path):
         # Check if the file ends with the ".sub" extension
         if file.endswith(".sub"):
