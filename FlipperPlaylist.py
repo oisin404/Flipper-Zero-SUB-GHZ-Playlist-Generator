8be40fc257f3ed422bdd730a03876dd0e7fa5a97
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 19:12:32 +0000

    Update FlipperPlaylist.py

--- FlipperPlaylist.py
+++ FlipperPlaylist.py
@@ -3,8 +3,8 @@ import os
 
 def main():
   path = str(input("Path For The Folder: "))
-  name = str(input("SUB-GHZ Playlist Name: "))
   flipper_path = str(input("Folder Inside Your SUB-GHZ Folder: "))
+  name = str(input("SUB-GHZ Playlist Name: "))
   file_end = ".txt"
   playlist = open((f"{name}{file_end}"), "wt")
   playlist.write("# " + name + "\n")
