025acc09e3c228948f234475e35571f4ae5d4e90
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 21:35:18 +0000

    optimize

--- FlipperPlaylist.py
+++ FlipperPlaylist.py
@@ -1,16 +1,27 @@
 # Flipper Zero SUB-GHZ Playlist Generator
 import os
 
-def main():
-  path = str(input("-------------------------\n---- Path For The Folder: "))
-  flipper_path = str(input("-------------------------\n---- Folder Inside Your SUB-GHZ Folder: "))
-  name = str(input("-------------------------\n---- SUB-GHZ Playlist Name: "))
-  file_end = ".txt"
-  playlist = open((f"{name}{file_end}"), "wt")
-  playlist.write("# " + name + "\n")
-  for f in os.listdir(path):
-    if f.endswith(".sub"):
-      playlist.write("sub: /ext/subghz/" + flipper_path + "/" + f + "\n")
-  print("-------------------------\n---- Playlist Created Successfully!")
 
-main()
+def test():
+    # Prompt the user to enter a path for the folder
+    folder_path = input("Enter the path for the folder: ")
+    # Create the text file in the current working directory
+    playlist_file = open("playlist.txt", "w")
+    # Iterate through the files in the folder
+    playlist_file.write("# Playlist\n")
+    for file in os.listdir(folder_path):
+        # Check if the file ends with the ".sub" extension
+        if file.endswith(".sub"):
+            # Get the full path of the file
+            file_path = os.path.join(folder_path, file)
+            file_path = file_path.replace("\\", "/")
+            file_path = file_path.replace("E:", "ext")
+            # Write the name and path of the file to the text file
+            playlist_file.write(f"sub: {file_path}\n")
+    # Close the text file
+    playlist_file.close()
+    print(file_path)
+    print("Done!")
+
+
+test()
