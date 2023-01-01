85098111f24da12915192615bef0a8c1afa60307
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 18:44:50 +0000

    fix stuff

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# Flipper Zero SUB-GHZ Playlist Generator
 import os
-name = str(input("SUB-GHZ Playlist Name: "))
-path = str(input("Path For The Folder: "))
-file_end = ".txt"
-playlist = open((f"{name}{file_end}"), "wt")
-for f in os.listdir(path):
-  if f.endswith(".sub"):
-    playlist.write(f + os.linesep)
+
+def main():
+  path = str(input("Path For The Folder: "))
+  name = str(input("SUB-GHZ Playlist Name: "))
+  file_end = ".txt"
+  playlist = open((f"{name}{file_end}"), "wt")
+  for f in os.listdir(path):
+    if f.endswith(".sub"):
+      playlist.write(f + "\n")
+
+main()
