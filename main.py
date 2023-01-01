5f93b7cdd5cd93892fa93a06c0a8e61ba3dc8aa0
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 17:53:59 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Flipper Zero SUB-GHZ Playlist Creator
+import os
+
+def find_files():
+    path = str(input("Path For The Folder: "))
+    for files in os.listdir(path):
+        if files.endswith(".sub"):
+            return files
+
+
+def write_playlist(files):
+    name = str(input("SUB-GHZ Playlist Name: "))
+    file_end = ".txt"
+    playlist = open((f"{name}{file_end}"), "wt")
+    playlist.writelines(files)
+
+write_playlist(find_files())
