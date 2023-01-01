4a61292417c04720317f7a700dd09e4ebe0ea70b
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2023-01-01 19:22:22 +0000

    Update FlipperPlaylist.py

--- FlipperPlaylist.py
+++ FlipperPlaylist.py
@@ -2,14 +2,15 @@
 import os
 
 def main():
-  path = str(input("Path For The Folder: "))
-  flipper_path = str(input("Folder Inside Your SUB-GHZ Folder: "))
-  name = str(input("SUB-GHZ Playlist Name: "))
+  path = str(input("-------------------------\n---- Path For The Folder: "))
+  flipper_path = str(input("-------------------------\n---- Folder Inside Your SUB-GHZ Folder: "))
+  name = str(input("-------------------------\n---- SUB-GHZ Playlist Name: "))
   file_end = ".txt"
   playlist = open((f"{name}{file_end}"), "wt")
   playlist.write("# " + name + "\n")
   for f in os.listdir(path):
     if f.endswith(".sub"):
       playlist.write("sub: /ext/subghz/" + flipper_path + "/" + f + "\n")
+  print("-------------------------\n---- Playlist Created Successfully!")
 
 main()
